630c330ba1a9e703e208bac8e01132993bace6e8
Albert Rocha <rocharocharocha95@gmail.com>
2022-05-24 06:34:19 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,30 @@
+const articles = [
+    {
+      id: 1,
+      title: 'the WET Codbase',
+      date: new Date(2020, 9, 4),
+    //   length: 11,
+      snippet: `For I know the plans I have for you,” declares the LORD, “plans to prosper you and not to harm you, plans to give you hope and a future.`,
+    },
+    {
+      id: 2,
+      title: 'goodby, clean code',
+      date: new Date(2019, 10, 22),
+    //   length: 5,
+      snippet: `Can we make it shorter? we don't need a contract, do we we are a non-profit organization. Can you make it faster? start on it today and we will talk about what i want next time . That sandwich needs to be more playful pass the mayo, appeal to the client, sue the vice president , can you turn it around in photoshop so we can see more of the front that will be a conversation piece yet can we barter services?.`,
+    },
+    {
+      id: 3,
+      title: 'my decade in review',
+      date: new Date(2018, 7, 11),
+    //   length: 5,
+      snippet: `Direct trade shabby chic four dollar toast, tilde actually try-hard bicycle rights aesthetic forage. Meditation keytar asymmetrical tacos artisan truffaut. Pabst jean shorts roof party scenester.`,
+    },
+    {
+      id: 4,
+      title: 'what are the react team principles',
+      date: new Date(2015, 5, 4),
+    //   length: 5,
+      snippet: `and use a kpop logo that's not a kpop logo! ugh can we have another option it's great, can you add a beard though can we try some other colours maybe. Make it sexy can we barter services? nor use a kpop logo that's not a kpop logo! ugh for I want you to take it to the next level im not sure, try something else. That sandwich needs to be more playful try a more powerful colour, `,
+    },
+  ];
